5f6bc979217872cb18996e7e502c48981bfa673d
Adrianxx1 <triaxis12q@gmail.com>
2023-09-11 11:57:47 -0600

    Strings 4

---
+++ strings.js
@@ -0,0 +1,34 @@
+// String en JavaScripts 
+
+let miString = "Este es un texto" + "de prueba";
+console.log(miString);
+
+console.log(miString.charAt(5));
+console.log(miString.endsWith(a));
+console.log(miString.length);
+console.log(miString.repeat(2));
+console.log(miString.toLowerCase());
+console.log(miString.toUpperCase());
+
+
+console.log("I' m Frank");
+console.log('I\'m Frank');
+
+//String multilinea
+const menu = `
+1 - Opcion 1
+2 - Opcion 2
+3 - Opcion 3
+`;
+
+console.log(menu);
+
+// String Templates
+// placeholder ${}
+
+const name = 'Peter';
+const supehero = 'Spiderman';
+const age = 20;
+
+console.log("Soy " + name + " soy " + superhero + ' y tengo ' + age + 'años');
+console.log("Soy ${name}  soy  ${superhero}   y tengo  ${age} años");
